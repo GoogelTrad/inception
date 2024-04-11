cf34c92e506b70740106be9f2e2d83754152c4f6
GoogelTrad <pooba@DESKTOP-AIG5DM6>
2024-04-11 19:00:20 +0200

    maria et nginx refait

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,3 +1,14 @@
-service mysql start;
+service mysql start 
 
-mysql -e "CREATE USER
+
+echo "CREATE DATABASE IF NOT EXISTS toto ;" > db1.sql #$db1_name
+echo "CREATE USER IF NOT EXISTS 'tata'@'%' IDENTIFIED BY 'titi' ;" >> db1.sql # $db1_user $db1_pwd
+echo "GRANT ALL PRIVILEGES ON toto.* TO 'tata'@'%' ;" >> db1.sql #$db1_name $db1_user
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
