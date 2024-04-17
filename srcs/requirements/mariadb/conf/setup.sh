2d29a486330e59a581ce119daf78834cd0fa35cd
GoogelTrad <celianmichez@free.fr>
2024-04-17 04:31:07 +0200

    docker compose, mariadb marche pas

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 service mysql start 
 
 
-echo "CREATE DATABASE IF NOT EXISTS toto ;" > db1.sql #$db1_name
-echo "CREATE USER IF NOT EXISTS 'tata'@'%' IDENTIFIED BY 'titi' ;" >> db1.sql # $db1_user $db1_pwd
-echo "GRANT ALL PRIVILEGES ON toto.* TO 'tata'@'%' ;" >> db1.sql #$db1_name $db1_user
+echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
---
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mkdir /var/www/
+mkdir /var/www/html
+
+cd /var/www/html
+
+rm -rf *
+
+#dowload wordpress (php-archive) from the git
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar 
+
+#download the lastest version of wordpress
+mv wp-cli.phar /usr/local/bin/wp
+wp core download --allow-root
+
+
+#change line in wpconfig to connect with the database
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sed -i -r "s/database/$db_name/1"   wp-config.php
+sed -i -r "s/database_user/$db_user/1"  wp-config.php
+sed -i -r "s/passwod/$db_pwd/1"    wp-config.php
+sed -i -r "s/localhost/mariadb/1"    wp-config.php  (to connect with mariadb database)
+
+
+#install wordpress and sets up the basic config + creates new user + install astra theme for the site
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+wp theme install astra --activate --allow-root
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir /run/php
+wp redis enable --allow-root
+
+#starts php service in the background
+/usr/sbin/php-fpm7.3 -F
